a63fbd91ac5bc84a1dc7c6bc72417b8306e16bcf
iqbalfarhan <iqbalfarhan1996@gmail.com>
2021-12-02 14:18:45 +0800

    show all rekanan landing

--- resources/views/webpage/rekanan.blade.php
+++ resources/views/webpage/rekanan.blade.php
@@ -3,7 +3,7 @@
 
 		<div class="row justify-content-center" data-aos="zoom-in">
 
-			@foreach (App\Rekanan::get()->take(6) as $rekan)
+			@foreach (App\Rekanan::get() as $rekan)
 			<div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center" title="{{ $rekan->nama }}" data-toggle="tooltip">
 				<img src="{{ url($rekan->gambar) }}" class="img-fluid">
 			</div>
